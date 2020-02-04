48e377b94714ccf19d44c2d24fb0821bd58de784
hvdthong <hvdthong@gmail.com>
2020-02-04 16:03:11 +0800

    add adv

--- adversarial_example.py
+++ adversarial_example.py
@@ -3,6 +3,12 @@ import tensorflow as tf
 from keras import backend
 import keras
 from keras.datasets import mnist, cifar10
+from keras.models import load_model
+from art.classifiers import KerasClassifier
+import numpy as np
+
+CLIP_MIN = -0.5
+CLIP_MAX = 0.5
 
 if __name__ == '__main__':
     """Generate the adversarial examples for mnist and cifar10 datasets
@@ -18,27 +24,37 @@ if __name__ == '__main__':
     parser.add_argument("--d", "-d", help="Dataset", type=str, default="mnist")
     parser.add_argument("--attack", "-attack", help="Define Attack Type", type=str, default="fgsm")
     args = parser.parse_args()
-    assert args.d in ["mnist", "cifar"], "Dataset should be either 'mnist' or 'cifar'"    
+    assert args.d in ["mnist", "cifar"], "Dataset should be either 'mnist' or 'cifar'"
+    assert args.attack in ["fgsm", "bim-a", "bim-b", "jsma", "c+w"], "Attack we should used"
     print(args)
 
-    # Force TensorFlow to use single thread to improve reproducibility
-    config = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
-
-    if keras.backend.image_data_format() != 'channels_last':
-        raise NotImplementedError("this tutorial requires keras to be configured to channels_last format")
-
-    # Create TF session and set as Keras backend session
-    sess = tf.Session(config=config)
-    keras.backend.set_session(sess)
-
     if args.d == 'mnist':
-        (x_train, y_train), (x_test, y_test) = mnist.load_data()
-        x_train = x_train.reshape(-1, 28, 28, 1)
-        x_test = x_test.reshape(-1, 28, 28, 1)        
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()        
+        x_test = x_test.reshape(-1, 28, 28, 1)
+        
+        x_test = x_test.astype("float32")
+        x_test = (x_test / 255.0) - (1.0 - CLIP_MAX)
 
+        # load pre-trained model
         model = load_model('./model/mnist_model_improvement-235-0.99.h5')
-        classifier = KerasClassifier(model=model, clip_values=(0, 255), use_logits=False)
+        classifier = KerasClassifier(model=model, clip_values=(-0.5, 0.5), use_logits=False)
 
     if args.attack == 'fgsm':
+        attack = FastGradientMethod(classifier=classifier, eps=0.3, batch_size=64)
+        x_test_adv = attack.generate(x=x_test)
+
+    if args.attack == '':
         print('hello')
-        
+    
+    # Accuracy of our test data
+    pred = np.argmax(classifier.predict(x_test), axis = 1)
+    acc =  np.mean(np.equal(pred, y_test.reshape(-1)))
+
+    print("The normal validation accuracy is: {}".format(acc))
+
+    adv_pred = np.argmax(keras_model.predict(x_test_adv), axis = 1)
+    adv_acc =  np.mean(np.equal(adv_pred, y_test))
+
+    print("The adversarial validation accuracy is: {}".format(adv_acc))
+    print(x_test_adv.shape)
+

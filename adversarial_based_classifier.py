c1cf84f04e606d27a3afaf2531f473a811e5a035
hvdthong <hvdthong@gmail.com>
2020-02-06 19:47:18 +0800

    add adv

--- adversarial_based_classifier.py
+++ adversarial_based_classifier.py
@@ -1,5 +1,12 @@
 import argparse
+import numpy as np
+from utils import load_file, convert_list_number_to_float
 
+
+def generate_labeled_data(x_adv, x_test):
+    """Generate labeled data for adversarial examples and test dataset
+       1 if an example is adversarial; otherwise 0
+    """
 
 def classify_adv_based_metrics(x_adv, x_test, args):
     """Calculate the performance of the adversarial examples based on different metrics
@@ -20,6 +27,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--d", "-d", help="Dataset", type=str, default="mnist")
     parser.add_argument('--alg', '-alg', help="Algorithm Classification", type=str, default="lr")
+    parser.add_argument('--n_fold', '-n_fold', help="Number of folds", type=int, default=10)
     parser.add_argument(
         "--clf_dsa", "-clf_dsa", help="Classification based on Distance-based Surprise Adequacy", action="store_true"
     )
@@ -31,12 +39,15 @@ if __name__ == '__main__':
     """
     parser.add_argument("--attack", "-attack", help="Define Attack Type", type=str, default="fgsm")
     args = parser.parse_args()
+
     assert args.d in ["mnist", "cifar"], "Dataset should be either 'mnist' or 'cifar'"
     assert args.alg in ["lr"], "Algorithm Classification"
     assert args.attack in ["fgsm", "bim", 'jsma', 'c+w'], "Dataset should be either 'fgsm', 'bim', 'jsma', 'c+w'"
     assert args.clf_dsa, "Select classification based on metrics"
     print(args)
 
-    if args.clf_dsa:
-        if args.attack:
-            print(args.attack)
+    if args.clf_dsa:        
+        if args.d == 'mnist':
+            x_adv = convert_list_number_to_float(load_file('./metrics/{}_adv_dsa_{}_activation_3.txt'.format(args.d, args.attack)))
+            x_test = convert_list_number_to_float(load_file('./metrics/{}_dsa_activation_3.txt'.format(args.d)))
+            classify_adv_based_metrics(x_adv=x_adv, x_test=x_test, args=args)
